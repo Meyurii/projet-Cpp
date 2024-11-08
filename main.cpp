ac11adb0d51853574938b4cf9d5ab70a604eb230
Meyurii <alicia.martin-prudhon@supinfo.com>
2024-11-08 17:07:15 +0100

    Le main est ajouté, elle inisialise un player et apelle la fonction qui met de la couleur

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Player.hpp"
+
+int main()
+{
+    // creer une classe Game ou je ne sais quel nom qui contient un la carte et un tableau de player
+
+    // Recupérer le nombre jouers
+    // Boucle le nombre de fois necesssaire afin d'ajouter chaque nouveau player
+    // Créer la carte en focntion du nombre de player
+    // faire une boucle while pour le nombre de tour avec boucle for pour chauqe jouer pour chaque tour
+    Player player;
+    std::string text = "bhevbehbvhbvehbvre";
+    player.show_text(text);
+    player.displayPlayerInfo();
+    return 0;
+}
