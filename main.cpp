0df3b391daca733985f8e3a674728bd85a697495
Meyurii <alicia.martin-prudhon@supinfo.com>
2024-11-08 19:10:54 +0100

    Mise a jour de main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
+#include "Game.hpp"
 #include "Player.hpp"
+
 
 int main()
 {
-    // creer une classe Game ou je ne sais quel nom qui contient un la carte et un tableau de player
 
-    // Recupérer le nombre jouers
-    // Boucle le nombre de fois necesssaire afin d'ajouter chaque nouveau player
-    // Créer la carte en focntion du nombre de player
     // faire une boucle while pour le nombre de tour avec boucle for pour chauqe jouer pour chaque tour
-    Player player;
-    std::string text = "bhevbehbvhbvehbvre";
-    player.show_text(text);
-    player.displayPlayerInfo();
+    Game game;
+    // std::string text = "bhevbehbvhbvehbvre";
+    // player.show_text(text);
+    // player.displayPlayerInfo();
     return 0;
 }
